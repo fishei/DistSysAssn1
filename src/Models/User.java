ddd2803b43adbbeabf2453fd47442db109966ace
fishei <fishei@rpi.edu>
2017-10-01 18:55:04 -0400

    initial commit, added some models

---
+++ src/Models/User.java
@@ -0,0 +1,11 @@
+package Models;
+
+public class User {
+    private String userName;
+    private int userId;
+
+    public User(String userName, int userId){
+        this.userName = userName;
+        this.userId = userId;
+    }
+}
